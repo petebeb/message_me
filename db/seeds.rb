579989b04905846bc5ff43e62469f75465104427
petebeb <petebeb@gmail.com>
2018-12-29 01:53:38 -0500

    Add messages model, partial, and updated index

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,9 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(username: "Jerry", password: "password")
+User.create(username: "George", password: "password")
+User.create(username: "Kramer", password: "password")
+User.create(username: "Elaine", password: "password")
+User.create(username: "Puddy", password: "password")
